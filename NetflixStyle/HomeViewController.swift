d33677654ca2cf8d2007ef4c18a1bc34d8e7cf47
soduma <soduma@naver.com>
2021-12-03 17:04:47 +0900

    basic section ui and darkmode always

--- NetflixStyle/HomeViewController.swift
+++ NetflixStyle/HomeViewController.swift
@@ -31,6 +31,7 @@ class HomeViewController: UICollectionViewController {
         //content cell 설정
         collectionView.register(ContentCollectionViewCell.self, forCellWithReuseIdentifier: "ContentCollectionViewCell")
         collectionView.register(ContentCollectionViewHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "ContentCollectionViewHeader")
+        collectionView.collectionViewLayout = layout()
     }
     
     func getContents() -> [Content] {
@@ -39,24 +40,64 @@ class HomeViewController: UICollectionViewController {
               let list = try? PropertyListDecoder().decode([Content].self, from: data) else { return [] }
         return list
     }
+    
+    //각각의 섹션 타입에 대한 uicollectionViewLayout 생성
+    private func layout() -> UICollectionViewLayout {
+        return UICollectionViewCompositionalLayout { [weak self] sectionNumber, environment -> NSCollectionLayoutSection? in
+            guard let self = self else { return nil }
+            
+            switch self.contents[sectionNumber].sectionType {
+            case .basic:
+                return self.createBasicTypeSection()
+            default:
+                return nil
+            }
+        }
+    }
+    
+    private func createBasicTypeSection() -> NSCollectionLayoutSection {
+        //item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.3), heightDimension: .fractionalHeight(0.75))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = .init(top: 10, leading: 5, bottom: 0, trailing: 5)
+        //group
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.9), heightDimension: .estimated(200))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 3)
+        //section
+        let section = NSCollectionLayoutSection(group: group)
+        section.orthogonalScrollingBehavior = .continuous
+        section.contentInsets = .init(top: 0, leading: 5, bottom: 0, trailing: 5)
+        
+        let sectionHeader = self.createSectionHeader()
+        section.boundarySupplementaryItems = [sectionHeader]
+        return section
+    }
+    
+    //section header layout 설정
+    private func createSectionHeader() -> NSCollectionLayoutBoundarySupplementaryItem {
+        let layoutSectionHeaderSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(30))
+        let sectionHeader = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: layoutSectionHeaderSize, elementKind: UICollectionView.elementKindSectionHeader, alignment: .top)
+        return sectionHeader
+    }
 }
 
 extension HomeViewController {
     
     // MARK: UICollectionViewDataSource
-
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         return contents.count
     }
 
-
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        switch section {
-        case 0:
-            return 1
-        default:
-            return contents[section].contentItem.count
+        if contents[section].sectionType == .basic {
+            switch section {
+            case 0:
+                return 1
+            default:
+                return contents[section].contentItem.count
+            }
         }
+        return 0
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -83,7 +124,6 @@ extension HomeViewController {
     }
 
     // MARK: UICollectionViewDelegate
-
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let sectionName = contents[indexPath.section].sectionName
         print("test!!!! \(sectionName) 섹션의 \(indexPath.row + 1) 콘텐츠 (+1된 값)")
@@ -93,7 +133,7 @@ extension HomeViewController {
 //swiftui preview
 struct HomeViewControllerPreview: PreviewProvider {
     static var previews: some View {
-        Container().preferredColorScheme(.dark).edgesIgnoringSafeArea(.all)
+        Container().edgesIgnoringSafeArea(.all)
     }
 
     struct Container: UIViewControllerRepresentable {
@@ -102,10 +142,10 @@ struct HomeViewControllerPreview: PreviewProvider {
             let homeViewController = HomeViewController(collectionViewLayout: layout)
             return UINavigationController(rootViewController: homeViewController)
         }
-
+        
         func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
-
         }
+        
         typealias UIViewControllerType = UIViewController
     }
 }
